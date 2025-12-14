4496dc1fe25d66ba4b1a9354b20f5e13df49de93
Layali <layalishkokani@gmail.com>
2025-12-13 18:55:08 -0600

    Update Entry model

--- src/model/Entry.java
+++ src/model/Entry.java
@@ -1,36 +1,42 @@
-//test change
+// test change
 
 package model;
 import java.io.Serializable;
 
-public class Entry implements Serializable{
+public class Entry implements Serializable {
     private String service;
     private String username;
     private String password;
     private String notes;
+
     public Entry(String service, String username, String password, String notes){
         this.service = service;
         this.username = username;
         this.password = password;
         this.notes = notes;
     }
+
     public String getService(){
         return service;
     }
-        public String getUsername(){
-            return username;
-        }
-        public String getPassword(){
-            return password;
-        }
-        public String getNotes(){
-            return notes;
-        }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public String getNotes(){
+        return notes;
+    }
+
+    @Override
     public String toString(){
-        System.out.println(entry);
-        return "Service: "+ service+
-        "\nUsername: "+username+
-        "\nPassword: "+ password+
-        "\nNotes: "+ notes;
+        return "Service: " + service +
+               "\nUsername: " + username +
+               "\nPassword: " + password +
+               "\nNotes: " + notes;
     }
-    }
+}
